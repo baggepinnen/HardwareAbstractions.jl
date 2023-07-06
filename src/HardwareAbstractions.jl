21006a3ae156d82825a871f4fd3c4018381892a5
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2023-07-06 16:01:34 +0200

    add compat

--- src/HardwareAbstractions.jl
+++ src/HardwareAbstractions.jl
@@ -12,7 +12,6 @@ import ControlSystemsBase: sampletime, isstable, ninputs, noutputs, nstates
 include("utilities.jl")
 
 include("interface.jl")
-include("interface_documentation.jl")
 include("reference_generators.jl")
 include("controllers.jl")
 
